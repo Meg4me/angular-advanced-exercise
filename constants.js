d4e5f7df2e55b326d059362f323d3b69a6b6915c
Mecha-Link7 <efbmachine06@gmail.com>
2024-08-26 16:50:00 +0200

    changement de taille_fenetre et texte principal

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
-export const taille_fenetre = "petite";
+export const taille_fenetre = "grande";
 export const alignement_fenetre = "";
-export const texte_principal = "Hello World";
+export const texte_principal = "Hello World " + Date();
 export const fond_decran = "jaune";
 export const couleur_texte_principal = "";
 export const couleur_fond = "cyan";
