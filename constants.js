801c0ec9a4ce7648afb678facd1229da164fdc8d
keiff3r <keiffer@blooo.io>
2024-08-26 13:09:23 +0200

    C5 : fond d’écran en jaune

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 export const taille_fenetre = "petite";
 export const alignement_fenetre = "";
 export const texte_principal = "Hello World";
-export const fond_decran = "";
+export const fond_decran = "jaune";
 export const couleur_texte_principal = "";
 export const couleur_fond = "";
