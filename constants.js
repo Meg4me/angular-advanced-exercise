d043f8025cb04da54917d1443460b6c0a5440b6c
keiff3r <keiffer@blooo.io>
2024-08-26 13:09:06 +0200

    C3 : mise à jour taille fenetre et ajustement du texte des constantes

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
-export const taille_fenetre = "";
+export const taille_fenetre = "petite";
 export const alignement_fenetre = "";
-export const texte_principal = "";
+export const texte_principal = "Hello World";
 export const fond_decran = "";
 export const couleur_texte_principal = "";
 export const couleur_fond = "";
